9ab40d6e1d55e97843df052430ad12f1cd386e4b
khalid-zein <zeinkhalid004@gmail.com>
2022-12-15 20:31:05 +0300

    completed the challenge

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # flatdango
-This is a web application that allows a user to purchase movie tickets from the theater.
+
+## Description
+
+This is a web application that allows a user to purchase movie tickets from the theater and also it shows movies available at that day and status of availabilty of tickets and time it will be played and duration.
+
+## Live Page
+
+https://khalid-zein.github.io/flatdango/
+
+## installation
+
+1. Open terminal(Ctrl+Alt+T)
+
+2. git clone        
+
+        git@github.com:khalid-zein/flatdango.git
+
+3. flatdango
+
+4. code . or atom . depending on the text editor of your choice.
+
+5. install json package 
+
+        npm install -g json-server
+
+6. run backend server in terminal 
+
+        json-server --watch db.json
+
+## Technologies Used
+
+1. [HTML](https://)
+2. [CSS](https://)
+3. [Javascript](https://)
+
+## Screenshot of live page
+<img src ="./img/screenshot1.png">
+<img src ="./img/screenshot2.png">
+
+## BDD
+
+|Behaviour                         | Input                                 |                                                 output|
+| ---------------------------------| ------------------------------------- | ----------------------------------------------------- |
+| click(Any upcoming movie listed) |                                       |   output movie detail icluding name playtime ...(etc) |
+| click(Buy Festival Tickets)      |                                       |   it shows you number of tickets remaining after your purchase, your ticket number and brief description of the movie you want to watch|
+
+## Author:
+
+[Khalid Zein](https:yueyyt)
+
+
+## Contact
+
++254794794734
+
+Email: zeinkhalid004@gmail.com
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://khalid-zein.github.io/flatdango/db.json")
+  fetch("https://khalid-zein.github.io/flatdango/db.json")
     .then((res) => res.json())
     .then((data) => {
       movieList(data.films);
     });
-    
-    function movieList(data){
-      data.forEach(movie => {
-        const markUp = `<li id ="listing">${movie.title}</>`;
-        
-        document.querySelector('ul').insertAdjacentHTML('beforeend', markUp);
-        
-        
-        // display first movie details
-        displayMovie(movie);
-        
-        
-        // display movie details on click of the movie titles
-        const moviesAvailable = document.querySelector("ul#moviesTitle");
-        moviesAvailable.addEventListener("click", function() {
-          displayMovie(movie);
-        });
-      });
-    };
 
+  function movieList(data) {
+    data.forEach((movie) => {
+      const markUp = document.createElement('li')
+      markUp.classList.add(`listing${movie.id}`)
+      markUp.classList.add(`hello`)
+      markUp.innerHTML = movie.title
+
+
+      document.querySelector("ul").append(markUp)
+
+      // display first movie details
+      displayMovie(movie);
+
+      // display movie details on click of the movie titles
+      markUp.addEventListener("click", function () {
+        displayMovie(movie);
+      });
+    });
+  }
+
+  function displayMovie(movie) {
     const movieName = document.getElementById("name");
     const movieImage = document.getElementById("poster");
     const description = document.getElementById("description");
@@ -32,15 +34,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const capacity = document.getElementById("capacity");
     const sold = document.getElementById("sold");
     const available = document.getElementById("available");
+    let availableTickets = movie.capacity - movie.tickets_sold;
 
-  function displayMovie(movie){
-        movieName.innerHTML = movie.title;
-        movieImage.src = movie.poster;
-        description.innerHTML = movie.description;
-        runTime.innerHTML = movie.runtime + " min";
-        showTime.innerHTML = movie.showtime;
-        capacity.innerHTML = movie.capacity + " seats";
-        sold.innerHTML = movie.tickets_sold + " tickets";
-        available.innerHTML = movie.capacity - movie.tickets_sold + " tickets";
-  };
+    movieName.innerHTML = movie.title;
+    movieImage.src = movie.poster;
+    description.innerHTML = movie.description;
+    runTime.innerHTML = movie.runtime + " min";
+    showTime.innerHTML = movie.showtime;
+    capacity.innerHTML = movie.capacity + " seats";
+    sold.innerHTML = movie.tickets_sold + " tickets";
+    available.innerHTML = availableTickets + " tickets";
+
+    husna(movie);
+  }
+
+  function husna(movie){
+    const alwy = document.getElementById("ticket");
+    const show = document.getElementById("show")
+    const run = document.getElementById("run");
+    const fat = document.getElementById("fat");
+    let remaining = (movie.capacity - movie.tickets_sold) - 1;
+
+    alwy.innerHTML = "Movie: " + movie.title;
+    show.innerHTML = "Showtime: " + movie.showtime;
+    run.innerHTML = "Runtime: " + movie.runtime + "min";
+    fat.innerHTML = remaining + " more tickets available you can purchase them on home page";
+  }
+});
+$(document).ready(function(){
+  $("#btn").click(function(){
+        $(".details").hide();
+        $(".koz").show();
+  });
 });
